8239a810b4011c3944140789f2a062000b962908
stepoleggg <olegstefanoyv1@gmail.com>
2019-08-16 16:40:06 +0500

    add saw, square waves

--- main.py
+++ main.py
@@ -8,11 +8,24 @@ p = pyaudio.PyAudio()
 #базовые параметры
 fs = 44100 #сэмпл рейт
 buffer_size = 128 #размер буфера в сэмплах
-volume_master = 0.3 #коэффициент громкости для мастер-канала
+volume_master = 0.2 #коэффициент громкости для мастер-канала
 
 #массив значений нот, содержащий громкости нажатия, фазы каждой ноты и громкости нажатия при уже отпущенной клавише 
 #(используется чтобы избежать щелчка)
 keys = np.zeros((128,3))
+
+#функции различных типов волн
+#square wave
+def square(buffer):
+    return buffer % 2*np.pi < np.pi
+#saw wave
+saw = lambda x: (x % 2*np.pi)/2
+#sin wave
+sin = np.sin
+
+#функция осциллятора, период 2Pi радиан
+def osc(buffer):
+    return sin(buffer)
 
 #расчет синусоид в буфере
 def callback(in_data, frame_count, time_info, status):
@@ -27,7 +40,7 @@ def callback(in_data, frame_count, time_info, status):
             f = 440*((2**(1.0/12))**(key-69))
             phase = v[1]
             #получаем буфер для одной ноты
-            d = (np.sin(2*np.pi*np.arange(phase*buffer_size, (phase+1)*buffer_size)*f/fs)*v[0]).astype(np.float32)
+            d = (osc(2*np.pi*np.arange(phase*buffer_size, (phase+1)*buffer_size)*f/fs)*v[0]).astype(np.float32)
             keys[key][1] = phase+1
             #добавляем буфер к общему буферу всех нот
             buffer = np.add(buffer,d)
@@ -36,15 +49,15 @@ def callback(in_data, frame_count, time_info, status):
             f = 440*((2**(1.0/12))**(key-69))
             phase = v[1]
             #расчет такой фазы, чтобы синусоида попала в ноль, и не было щелчка
-            x = int((0.5 - phase*buffer_size*f/fs % 0.5)*fs/f)
+            x = int((1 - phase*buffer_size*f/fs % 1)*fs/f)
             #print(x, (phase*buffer_size+x)*f/fs)
             #если частота низкая и нельзя закончить синусоиду в пределах буфера
             if x+1 > buffer_size:
-                d = (np.sin(2*np.pi*np.arange(phase*buffer_size, phase*buffer_size+buffer_size)*f/fs)*v[2]).astype(np.float32)
+                d = (osc(2*np.pi*np.arange(phase*buffer_size, phase*buffer_size+buffer_size)*f/fs)*v[2]).astype(np.float32)
                 keys[key][1] = phase+1
             #заканчиваем в пределах буфера
             else:
-                d1 = (np.sin(2*np.pi*np.arange(phase*buffer_size, phase*buffer_size+x+1)*f/fs)*v[2]).astype(np.float32)
+                d1 = (osc(2*np.pi*np.arange(phase*buffer_size, phase*buffer_size+x+1)*f/fs)*v[2]).astype(np.float32)
                 d2 = np.zeros(buffer_size-x-1, dtype = np.float32)
                 d = np.concatenate((d1,d2))
                 keys[key][1] = 0
